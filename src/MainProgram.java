7a9650be950f966ac9b74ebe3b38be16d63e8d79
nasimju <Arm@AVA.local>
2015-10-25 04:09:14 +0700

    insert the description

--- src/MainProgram.java
+++ src/MainProgram.java
@@ -4,6 +4,15 @@ public class MainProgram {
 	public static void main(String [] args){
 		System.out.println("Hello Git!");
 		System.out.println("This time test to add java project to git");
+		System.out.println("Step[1] : create java project and/or source code");
+		System.out.println("Step[2] : commit your code to create master");
+		System.out.println("Step[3] : if you  connect the internet, you can publish your code");
+		System.out.println("Step[4] : You can change your code and then commit to the master");
+		System.out.println("Note : before commition, please insert the description of your change");
+		System.out.println("Step[] : ");
+		System.out.println("Step[] : ");
+		System.out.println("Step[] : ");
+		System.out.println("Step[] : ");
 	}
 	
 }
