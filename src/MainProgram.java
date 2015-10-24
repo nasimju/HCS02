61ef4d649b6d6543dbcb5b7352be2a8db7d8f343
nasimju <Arm@AVA.local>
2015-10-25 04:01:16 +0700

    test to add java project to git

---
+++ src/MainProgram.java
@@ -0,0 +1,9 @@
+
+public class MainProgram {
+
+	public static void main(String [] args){
+		System.out.println("Hello Git!");
+		System.out.println("This time test to add java project to git");
+	}
+	
+}
